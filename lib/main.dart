b877058fc131998f5187f482eba1639701a0ffbc
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-25 04:54:51 +0530

    Added health provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quamin_health_module/diet_module/diet_providers/cart_provider.dart';
 import 'package:quamin_health_module/firebase_options.dart';
+import 'package:quamin_health_module/health_module/providers/health_provider.dart';
 import 'package:quamin_health_module/router.dart';
 import 'package:quamin_health_module/health_module/theme/app_theme.dart';
 import 'health_module/providers/meal_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-  
+
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();  
+  WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
   } catch (e) {
-    print('Firebase initialization error: $e'); 
+    print('Firebase initialization error: $e');
   }
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,6 +31,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => MealProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => HealthProvider()..initialize())
       ],
       child: MaterialApp.router(
         title: 'Health Tracker',
